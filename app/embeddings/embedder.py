fe3a965adbffe49553b94f2aa9de4217088945fb
sriraanjith5-AI <sriraanjith5@gmail.com>
2026-07-31 10:39:19 +0530

    Add initial embedding generator scaffolding

---
+++ app/embeddings/embedder.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+
+
+class Embedder:
+
+    def __init__(self):
+        self.model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    def embed(self, text):
+        return self.model.encode(text)
